d163a781ce36e3c7e8aeb9d0d67d8bb06823e8c8
pNre <mail@pierluigi.me>
2014-07-30 17:14:53 +0200

    LazySequence files

---
+++ ExSwift/LazySequence.swift
@@ -0,0 +1,13 @@
+//
+//  LazySequence.swift
+//  ExSwift
+//
+//  Created by pNre on 30/07/14.
+//  Copyright (c) 2014 pNre. All rights reserved.
+//
+
+import Foundation
+
+extension LazySequence {
+    
+}
---
+++ ExSwiftTests/ExSwiftLazySequenceTests.swift
@@ -0,0 +1,12 @@
+//
+//  ExSwiftLazySequenceTests.swift
+//  ExSwift
+//
+//  Created by pNre on 30/07/14.
+//  Copyright (c) 2014 pNre. All rights reserved.
+//
+
+import XCTest
+
+class ExSwiftLazySequenceTests: XCTestCase {
+}
