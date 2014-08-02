cfa8c27f752b0c01244b98c98f812d12a17e2a35
pNre <mail@pierluigi.me>
2014-08-02 16:43:54 +0200

    Test cases

--- ExSwiftTests/ExSwiftLazySequenceTests.swift
+++ ExSwiftTests/ExSwiftLazySequenceTests.swift
@@ -12,7 +12,7 @@ class ExSwiftLazySequenceTests: XCTestCase {
     
     let sequence = lazy(1...5) as LazySequence
     let emptySequence = lazy(1..<1) as LazySequence
-    
+
     func testFirst () {
         XCTAssertEqual(sequence.first()!, 1)
     }
@@ -26,7 +26,7 @@ class ExSwiftLazySequenceTests: XCTestCase {
     }
     
     func testSkipBeyondEnd () {
-        XCTAssertEqualArrays(sequence.skip(8).array, [])
+        XCTAssertTrue(sequence.skip(8).array.isEmpty)
     }
     
     func testSkipWhile () {
@@ -34,90 +34,66 @@ class ExSwiftLazySequenceTests: XCTestCase {
     }
     
     func testSkipWhileBeyondEnd () {
-        var skipped = SequenceOf(sequence).skipWhile { $0 < 20 }
-        XCTAssertEqualArrays(Array(skipped), [])
+        XCTAssertTrue(sequence.skipWhile { $0 < 20 }.array.isEmpty)
     }
     
     func testContains () {
-        XCTAssertTrue(SequenceOf(sequence).contains(1))
-        XCTAssertFalse(SequenceOf(sequence).contains(56))
+        XCTAssertTrue(sequence.contains(1))
+        XCTAssertFalse(sequence.contains(56))
     }
     
     func testTake () {
-        var take = SequenceOf(sequence).take(2)
-        XCTAssertEqualArrays(Array(take), [1, 2])
+        XCTAssertEqualArrays(sequence.take(2).array, [1, 2])
     }
     
     func testTakeBeyondSequenceEnd () {
-        var take = SequenceOf(sequence).take(20)
-        XCTAssertEqualArrays(Array(take), [1, 2, 3, 4, 5])
+        XCTAssertEqualArrays(sequence.take(20).array, [1, 2, 3, 4, 5])
     }
     
     func testTakeWhile () {
-        var take = SequenceOf(sequence).takeWhile { $0 != 3 }
-        XCTAssertEqualArrays(Array(take), [1, 2])
+        XCTAssertEqualArrays(sequence.takeWhile { $0 != 3 }.array, [1, 2])
     }
     
     func testTakeWhileConditionNeverTrue () {
-        var take = SequenceOf(sequence).takeWhile { $0 == 7 }
-        XCTAssertEqualArrays(Array(take), [])
+        XCTAssertTrue(sequence.takeWhile { $0 == 7 }.array.isEmpty)
     }
     
     func testTakeWhileConditionNotMet () {
-        var take = SequenceOf(sequence).takeWhile { $0 != 7 }
-        XCTAssertEqualArrays(Array(take), [1, 2, 3, 4, 5])
+        XCTAssertEqualArrays(sequence.takeWhile { $0 != 7 }.array, [1, 2, 3, 4, 5])
     }
     
     func testIndexOf () {
-        XCTAssertEqual(SequenceOf(sequence).indexOf(2)!, 1)
-        XCTAssertNil(SequenceOf(sequence).indexOf(77))
+        XCTAssertEqual(sequence.indexOf(2)!, 1)
+        XCTAssertNil(sequence.indexOf(77))
     }
     
     func testGet () {
-        XCTAssertEqual(SequenceOf(sequence).get(3)!, 3)
-        XCTAssertNil(SequenceOf(sequence).get(22))
+        XCTAssertEqual(sequence.get(3)!, 3)
+        XCTAssertNil(sequence.get(22))
     }
     
     func testGetRange () {
-        var subSequence = SequenceOf(sequence).get(1..<3)
-        XCTAssertEqualArrays(Array(subSequence), [2, 3])
-        
-        subSequence = SequenceOf(sequence).get(0..<0)
-        XCTAssertEqualArrays(Array(subSequence), [])
+        XCTAssertEqualArrays(sequence.get(1..<3).array, [2, 3])
+        XCTAssertTrue(sequence.get(0..<0).array.isEmpty)
     }
     
     func testGetRangeOutOfBounds () {
-        var subSequence = SequenceOf(sequence).get(10..<15)
-        XCTAssertEqualArrays(Array(subSequence), [])
+        XCTAssertTrue(sequence.get(10..<15).array.isEmpty)
     }
     
     func testAny () {
-        XCTAssertTrue(SequenceOf(sequence).any { $0 == 1 })
-        XCTAssertFalse(SequenceOf(sequence).any { $0 == 77 })
-    }
-    
-    func testFilter () {
-        var evens = SequenceOf(sequence).filter { $0 % 2 == 0 }
-        XCTAssertEqualArrays(Array(evens), [2, 4])
-        
-        var odds = SequenceOf(sequence).filter { $0 % 2 == 1 }
-        XCTAssertEqualArrays(Array(odds), [1, 3, 5])
-        
-        var all = SequenceOf(sequence).filter { $0 < 10 }
-        XCTAssertEqualArrays(Array(all), [1, 2, 3, 4, 5])
-        
-        var none = SequenceOf(sequence).filter { $0 > 10 }
-        XCTAssertEqualArrays(Array(none), [])
+        XCTAssertTrue(sequence.any { $0 == 1 })
+        XCTAssertFalse(sequence.any { $0 == 77 })
     }
     
     func testReject () {
-        var rejected = SequenceOf(sequence).reject { $0 == 3 }
-        XCTAssertEqualArrays(Array(rejected), [1, 2, 4, 5])
+        var rejected = sequence.reject { $0 == 3 }
+        XCTAssertEqualArrays(rejected.array, [1, 2, 4, 5])
         
-        rejected = SequenceOf(sequence).reject { $0 == 1 }
-        XCTAssertEqualArrays(Array(rejected), [2, 3, 4, 5])
+        rejected = sequence.reject { $0 == 1 }
+        XCTAssertEqualArrays(rejected.array, [2, 3, 4, 5])
         
-        rejected = SequenceOf(sequence).reject { $0 == 10 }
-        XCTAssertEqualArrays(Array(rejected), [1, 2, 3, 4, 5])
+        rejected = sequence.reject { $0 == 10 }
+        XCTAssertEqualArrays(rejected.array, [1, 2, 3, 4, 5])
     }
 }
